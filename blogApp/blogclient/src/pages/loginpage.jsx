4f61dcd340058f951d5e677bf129924566bba64b
purnatadikonda5 <purnatadikonda5@gmail.com>
2024-12-29 23:49:08 +0530

    used some usestate hooks to modify the username

--- blogApp/blogclient/src/pages/loginpage.jsx
+++ blogApp/blogclient/src/pages/loginpage.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react"
+
 export default function LoginPage(){ 
+    let [username,setusername]= useState('');
+    let [password,setpassword]= useState('');
     return (
         <form  className="login" method="post" action="http://localhost:8080/login">
             <h2>LOGIN</h2>
-            <input type="text" name="username" className="username" placeholder="Username" />
-            <input type="password" name="password" className="password" placeholder="Password"/>
+            <input type="text" name="username" className="username" value={username} onChange={ev=>setusername(ev.target.value)} placeholder="Username" />
+            <input type="password" name="password" className="password" value={password} onChange={ev=>setpassword(ev.target.value)} placeholder="Password"/>
             <button className="form-btn"> Login </button>
         </form>
     )
